08a9d06259db38b000f902a120f142f49bebafec
Candie Hill <can330330@yahoo.com>
2022-09-20 14:57:45 -0400

    fixing year-make-model sizing.

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -3,16 +3,18 @@ import './Car.css';
 function Car({ year, make, model, price, city, state, mileage, photo, color }){
     return (
         <div className="car">
-            <img className='car-photo' src={`${photo}`} alt={`${color} ${make} ${model}`}/>
-            <div className='car-title-price'>
-                <div className="car-year-make-model">{year} {make} {model}</div>
-                <div className="car-price">{price}</div>
-            </div>
-            <div className='car-milage'>{mileage}</div>
-            <div className='car-city-inventory'>
-                <div className='car-city-state'>{`${city},${state}`}</div>
-                <button className='car-track-price'>Track Price</button>
-                <button className='car-availability'>Check Availability</button>
+                <img src={`${photo}`} alt={`${color} ${make} ${model}`}/>  
+            <div className='car-details'>
+                {/* <div className='car-title-price'> */}
+                    <div className="car-year-make-model">{year} {make} {model}</div>
+                    <div className="car-price">{price}</div>
+                {/* </div> */}
+                <div className='car-milage'>{mileage}</div>
+                <div className='car-city-inventory'>
+                    <div className='car-city-state'>{`${city},${state}`}</div>
+                    <button className='car-track-price'>Track Price</button>
+                    <button className='car-availability'>Check Availability</button>
+                </div>                
             </div>
         </div>
     )
