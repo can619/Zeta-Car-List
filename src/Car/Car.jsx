3db5b9ea1822f258cd16567df52112c5a2e8286c
Candie Hill <can330330@yahoo.com>
2022-09-20 20:48:47 -0400

    added grid for the car-details

--- src/Car/Car.jsx
+++ src/Car/Car.jsx
@@ -1,20 +1,23 @@
 import './Car.css';
+import { AiOutlineHeart } from 'react-icons/ai';
 
 function Car({ year, make, model, price, city, state, mileage, photo, color }){
     return (
         <div className="car">
-                <img src={`${photo}`} alt={`${color} ${make} ${model}`}/>  
+            <div className='car-heart-img'>
+                <AiOutlineHeart className='car-heart'/>
+                <img src={`${photo}`} alt={`${color} ${make} ${model}`}/>                 
+            </div>
+ 
             <div className='car-details'>
-                {/* <div className='car-title-price'> */}
                     <div className="car-year-make-model">{year} {make} {model}</div>
                     <div className="car-price">{price}</div>
-                {/* </div> */}
+             
+                    <div className='car-estimate'>Estimate</div>
                 <div className='car-milage'>{mileage}</div>
-                <div className='car-city-inventory'>
                     <div className='car-city-state'>{`${city},${state}`}</div>
                     <button className='car-track-price'>Track Price</button>
-                    <button className='car-availability'>Check Availability</button>
-                </div>                
+                    <button className='car-availability'>Check Availability</button>               
             </div>
         </div>
     )
